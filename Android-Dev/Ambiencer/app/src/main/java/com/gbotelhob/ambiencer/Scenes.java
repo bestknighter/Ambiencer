6db37e7006efe1a0f8bad46938de099ca6efc939
Gabriel Filipe Botelho Barbosa <gabrielfbbarbosa@gmail.com>
2016-02-27 18:35:32 -0500

    Comecei a criar para aprender

--- Android-Dev/Ambiencer/app/src/main/java/com/gbotelhob/ambiencer/Scenes.java
+++ Android-Dev/Ambiencer/app/src/main/java/com/gbotelhob/ambiencer/Scenes.java
@@ -1,13 +1,22 @@
 package com.gbotelhob.ambiencer;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Scenes extends AppCompatActivity {
+
+    String[] fakeScenes = {"All", "Intro", "Forest", "Dungeon", "Castle"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_scene, fakeScenes);
+        ListView lv = (ListView)findViewById(R.id.listview_scenes);
+        lv.setAdapter(adapter);
+
     }
 }
